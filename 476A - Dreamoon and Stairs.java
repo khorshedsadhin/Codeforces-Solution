6d59357c6efa24800de5ac4216218db3196ee2a7
Khorshed Alam Sadhin <136317677+khorshedsadhin@users.noreply.github.com>
2024-01-20 18:56:51 +0600

    Create 476A - Dreamoon and Stairs.java

---
+++ 476A - Dreamoon and Stairs.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/*
+*/
+
+public class CompetitiveProgramming {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = fs.nextInt();
+        int m = fs.nextInt();
+
+        int mid = 1 + (n - 1)/2;
+
+        if (m > n) {
+            out.println(-1);
+        }
+
+        else {
+            for (int i = mid; i <= n; i++) {
+                if (i % m == 0) {
+                    out.println(i);
+                    break;
+                }
+            }
+        }
+        
+        out.flush();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+        int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int x : arr) {
+            System.out.println(x);
+        }
+    }
+    public static void sort(int[] arr) {
+        /*
+        Arrays.sort uses quicksort which is dumb
+        Collection.sort uses mergesort -> O(nlog n)
+        */
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int x : arr) {
+            al.add(x);
+        }
+        Collections.sort(al);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = al.get(i);
+        }
+    }
+    public static void ReverseSort(int[] arr) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int x : arr) {
+            al.add(x);
+        }
+        Collections.sort(al, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = al.get(i);
+        }
+    }
+}
